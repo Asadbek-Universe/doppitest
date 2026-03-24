35228becde0aa5dd822e9a236005bb51fc23c8c1
Asadbek <asadbekmahmudov3127@gmail.com>
2026-03-24 14:15:38 +0500

    Updates

---
+++ assets/chevron-left-D7MXgb98.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-Dnz9c0jM.js";const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ assets/chevron-right-d2D0an4V.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-Dnz9c0jM.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
